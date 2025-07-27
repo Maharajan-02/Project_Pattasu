bc16b8dbd122aa16ab8b2c16bcd8a372f03be5d5
Maharajan-02 <rajascientist98@gmail.com>
2025-07-27 15:28:36 +0530

    feat(project pattasu)
    1. Removed extra commit

--- src/main/java/com/pattasu/dto/ProductUploadRequest.java
+++ src/main/java/com/pattasu/dto/ProductUploadRequest.java
@@ -9,7 +9,6 @@ public class ProductUploadRequest {
     private double price;
     private int stockQuantity;
     private MultipartFile image;
-    private Boolean isActive;
     
 	public String getName() {
 		return name;
@@ -40,12 +39,6 @@ public class ProductUploadRequest {
 	}
 	public void setImage(MultipartFile image) {
 		this.image = image;
-	}
-	public Boolean getIsActive() {
-		return isActive;
-	}
-	public void setIsActive(Boolean isActive) {
-		this.isActive = isActive;
 	}
     
 }
